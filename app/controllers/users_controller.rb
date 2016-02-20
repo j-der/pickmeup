5e76adec7e14ddaada485caa732175a6ad5f0576
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-20 22:08:20 +0000

    added comments to react files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def index
+    # overwrite the default render of '/users/index' and render
+    # 'ParentComponent' with a property of users
     render component: 'ParentComponent', props: { users: User.all }
   end
 
