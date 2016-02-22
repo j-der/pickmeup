364fa43124c0b332279d8ab9c72f46d58f208e64
Jas Der <jasmineder@gmail.com>
2016-02-22 16:16:18 +0000

    create and new user methods defined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
     render component: 'ParentComponent', props: { users: User.all }
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to rides_path, notice: "Welcome to Pick Me Up, #{@user.first_name}! Buckle up, and let's go!"
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
+
 end
