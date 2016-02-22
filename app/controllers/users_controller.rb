f16b1dd61224af298551695f2ab52f8f0581f36c
Jas Der <jasmineder@gmail.com>
2016-02-22 22:21:48 +0000

    removed some old useless code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    render component: 'NewUser', props: { view: hidden }
+    render component: 'NewUser'
     @user = User.new(user_params)
 
     if @user.save
