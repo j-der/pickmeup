ea913870e00d670d0ae272f4d8952a8faf395912
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-22 17:41:49 +0000

    created basic user preview component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def index
     # overwrite the default render of '/users/index' and render
     # 'ParentComponent' with a property of users
-    render component: 'ParentComponent', props: { users: User.all }
+    # render component: 'ParentComponent', props: { users: User.all }
+    render component: 'UserTweet', props: { users: User.all }
   end
 
 end
