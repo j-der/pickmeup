ce13e318207b14f5185f025d16c7813db4a601d7
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-22 23:53:42 +0000

    created splash page. added search bar and nav

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -1,8 +1,12 @@
+//= require ./NewUser.js
+
 var NavBar = React.createClass({
 
   render: function(){
     return (
       <div className="nav-bar">
+        <NewUser />
+        <button className="log-in-button">Log In</button>
       </div>
     );
   }
