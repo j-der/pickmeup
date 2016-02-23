474ea0d6b8edad79fac411df8b473bd8031f5b84
tessakruger <tessarfoto@gmail.com>
2016-02-23 19:16:09 +0000

    post ride form toggles open/close on button click

--- app/assets/javascripts/components/PostRideForm.js.jsx
+++ app/assets/javascripts/components/PostRideForm.js.jsx
@@ -1,36 +1,58 @@
 var PostRideForm = React.createClass({
 
+  getInitialState: function() {
+    return {showForm: false}
+  },
+
   displayForm: function() {
-    return (
-      <form method="POST" action="/rides">
-        <div>
-          <label htmlFor="title">Your route: </label>
-            <input type="text" name="ride[title]" />
-        </div>
-        <div>
-          <label htmlFor="details">Details: </label>
-            <input type="textarea" name="ride[details]" />
-        </div>
-        <div>
-          <label>Number of seats available: </label>
-            <input type="number" min="1" max="6" name="ride[available_seats]"/>
-        </div>
-        <div>
-        <input type="hidden" name="ride[authenticity_token]" value={ this.props.authenticity_token } />
-        </div>
-        <div>
-          <button type="submit">Let&#39;s roll</button>
-        </div>
-      </form>
-    );
+  if (this.state.showForm) {
+      return (
+          <form method="POST" action="/rides" className="modal-content">
+            <div>
+              <label htmlFor="title">Your route: </label>
+                <input type="text" name="ride[title]" />
+            </div>
+            <div>
+              <label htmlFor="details">Details: </label>
+                <input type="textarea" name="ride[details]" />
+            </div>
+            <div>
+              <label>Number of seats available: </label>
+                <input type="number" min="1" max="6" name="ride[available_seats]"/>
+            </div>
+            <div>
+            <input type="hidden" name="ride[authenticity_token]" value={ this.props.authenticity_token } />
+            </div>
+            <div>
+              <button type="submit">Let&#39;s roll</button>
+            </div>
+          </form>
+
+      );
+    }
+  },
+
+  openModal: function() {
+    if (this.state.showForm) {
+      this.setState({
+        showForm: false
+      })
+    }
+    else {
+      this.setState({
+        showForm: true
+      })
+    }
   },
 
   render: function() {
+    thisOne = this;
     return (
       <div>
-        <button>Post a Ride</button>
+        <button id="myBtn" onClick={function() {thisOne.openModal()}}>Post a Ride</button>
+        {this.displayForm()}
       </div>
     );
   }
 
-});
+})
